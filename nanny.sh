dae46803982c07d95403811b7a32980d9acc1a56
Mingcong Bai <jeffbai@aosc.xyz>
2022-07-13 17:19:40 -0700

    nanny.sh: improve CPU baseline error message

--- nanny.sh
+++ nanny.sh
@@ -7,7 +7,7 @@ USE_GUI=1
 cpu_baseline_error() {
     if ! grep -P "^flags\s+: .+?\s($1)" /proc/cpuinfo; then
         local features="$1"
-        MESSAGE_TEXT="$(eval_gettext 'Your CPU does not support the "${features}" feature, therefore ${PKGNAME} will not work.')"
+        MESSAGE_TEXT="$(eval_gettext 'Your processor does not support the "${features}" feature, which is required by ${PKGNAME}. This application will now terminate.')"
         show
         exit 10
     fi
